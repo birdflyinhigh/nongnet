18525ad4e2f7075b58ff1865c3c63b0103351c61
taojian <taojian@zbj.com>
2018-06-03 10:57:14 +0800

    改成scrapy-redis之前

--- Nongnet/Nongnet/spiders/nongnet.py
+++ Nongnet/Nongnet/spiders/nongnet.py
@@ -3,17 +3,22 @@ import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
-
+from scrapy_redis.spiders import RedisCrawlSpider
 class NongnetSpider(CrawlSpider):
     name = 'nongnet'
     allowed_domains = ['nongnet.com']
-    start_urls = ['http://nongnet.com/']
-
+    start_urls = ['http://www.nongnet.com']
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        # 大分类页面url
+        Rule(LinkExtractor(allow=r'classi_[0-9]+\.aspx'), follow=True),
+        # 商品列表页url
+        Rule(LinkExtractor(allow=r'list_.+\.aspx'), follow=True),
+        # 商品url
+        Rule(LinkExtractor(allow=r'http://www.nongnet.com/xinxi/[0-9]+\.aspx'), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
+        print(response.url)
         i = {}
         #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
         #i['name'] = response.xpath('//div[@id="name"]').extract()
